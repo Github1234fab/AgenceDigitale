8ee30b036d2c6b0c7a63c93d7ed36b93850dad98
Github1234fab <fabien.marceau@gmail.com>
2023-12-17 12:12:11 +0100

    commit3

---
+++ src/sections/Csection.svelte
@@ -0,0 +1,85 @@
+<script>
+	import Robot from '../Assets/png-gralypho/Saly-39.png';
+</script>
+
+<section class="C-section">
+	<img src={Robot} alt="icon de robot en 3D" class="robot" />
+	<h1>Votre projet est <br /> unique et mérite <br /> une technologie <br /> appropriée.</h1>
+	<h2>
+		Selon vos besoins spécifiques, nous utiliserons choisirons méticuleusement la technologie la
+		plus adaptée.
+	</h2>
+	<p>
+		Wordpress? Wix? Shopify? From Scratch? Woo Commerce? Squarespace ? Webflow? (...) Quelle est la
+		technologie la plus appropriée à la réalisation de votre site internet ou de votre application?
+		C'est une question fondamentale parce qu'elle va déterminer la durée de conception, le coût et
+		l'efficacité de votre outil en terme de rapidité, de fonctionnalité et de maintenance
+	</p>
+	<div class="halo"></div>
+</section>
+
+<style>
+	.C-section {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-template-rows: auto;
+		height: 100%;
+		font-size: 16px;
+		background-color: var(--colorO);
+		margin-top: 100px;
+	}
+	.C-section h1 {
+		grid-column: 6/12;
+		grid-row: 1;
+		margin-left: 20px;
+		font-family: epilogue;
+		font-weight: 600;
+		font-size: 1.8em;
+		color: white;
+		text-transform: uppercase;
+	}
+	.C-section h2 {
+		grid-column: 6/12;
+		grid-row: 1;
+		margin-left: 20px;
+		margin-top: 120px;
+		font-family: epilogue;
+		font-weight: 200;
+		font-size: 1.2em;
+		color: var(--colorC);
+	}
+	.C-section p {
+		grid-column: 2/12;
+		grid-row: 2;
+		color: var(--colorC);
+		margin-top: 30px;
+		text-align: justify;
+		font-weight: 300;
+	}
+	.robot {
+		grid-column: 1/7;
+		grid-row: 1;
+		height: 180px;
+		width: 180px;
+		margin-left: -10px;
+		margin-top: -10px;
+		z-index: 1;
+	}
+	.halo{
+	grid-column: 1/7;
+	grid-row: 1;
+        background-color: rgb(79, 68, 235);
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        filter: blur(30px);
+        z-index:0;
+        margin-left: 30px;
+        margin-top: 50px;
+}
+	@media screen and (max-width: 768px) {
+		.C-section {
+			font-size: 10px;
+		}
+	}
+</style>
---
+++ src/sections/ESection.svelte
@@ -0,0 +1,70 @@
+<script>
+	import SpaceShip from '../Assets/png-gralypho/Saly-43.png';
+        import Banner from '../Components/Banner.svelte';
+</script>
+
+<section class="E-section">
+	<h1>Gralypho créer un service unique de produits numériques.</h1>
+	<h2>
+		Selon vos besoins, nous utiliserons Wix, ou wordpress, ou sqarspace, Shopify, woo Commerce (...).
+	</h2>
+	<img src={SpaceShip} alt="" class="space-ship" />
+	<div class="halo"></div>
+</section>
+
+<Banner/>
+
+<style>
+	.E-section {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-template-rows: auto;
+		margin-top: 170px;
+		height: 100%;
+		font-size: 16px;
+		background-color: var(--colorO);
+	}
+	.E-section h1 {
+		grid-column: 2/8;
+		grid-row: 1;
+		font-family: epilogue;
+		font-weight: 600;
+		font-size: 2em;
+		color: white;
+	}
+	.E-section h2 {
+		grid-column: 2/8;
+		grid-row: 1;
+		margin-top: 150px;
+		font-family: epilogue;
+		font-weight: 300;
+		font-size: 1.2em;
+		color: var(--colorC);
+	}
+        .space-ship{
+                grid-column: 8/12;
+                height: 150px;
+                width: 150px;
+		transform: rotate(50deg);
+		grid-row: 1;
+		z-index: 1;
+                
+        }
+		.halo {
+		grid-column: 7/12;
+		grid-row: 1;
+		background-color: rgb(79, 68, 235);
+		width: 100px;
+		height: 100px;
+		border-radius: 50%;
+		filter: blur(30px);
+		z-index: 0;
+		margin-left: 70px;
+		margin-top: 20px;
+	}
+		@media screen and (max-width: 768px) {
+		.E-section {
+			font-size: 10px;
+		}
+	}
+</style>
